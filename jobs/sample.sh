872127b1e0ef8c32c96c3e91d48d9a7ebeca37b3
Nathan Wong <44811859+nkw1200@users.noreply.github.com>
2022-03-23 21:30:10 -0400

    Update sample.sh

--- jobs/sample.sh
+++ jobs/sample.sh
@@ -3,6 +3,11 @@
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
 #SBATCH --time=36:00:00
+#SBATCH --cpus-per-gpu=32
+#SBATCH --mem-per-gpu=32GB
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=[INSERT EMAIL]
+#SBATCH --job-name=emojiComplete
 pip3 install matplotlib --user
 pip3 install numpy --user
 pip3 install scipy --user
